b87f159a7b36ad4d27417bc9d30ffb7472810856
angadgit <angadgupta214@gmail.com>
2022-10-15 16:38:05 +0530

    login layout change phone size

--- layout/layout.js
+++ layout/layout.js
@@ -4,12 +4,12 @@ import styles from '../styles/Layout.module.css'
 export default function Layout({ children }) {
   return (
     <div className='flex h-screen bg-blue-400'>
-      <div className='m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2'>
-        <div className={styles.imgStyle}>
+      <div className='m-5 md:m-auto bg-slate-50 rounded-md md:w-3/5 md:h-3/4 grid lg:grid-cols-2'>
+        <div className={`${styles.imgStyle}`}>
           {/* <div className={styles.cartoonImg}></div> */}
         </div>
         <div className='right flex flex-col justify-evenly'>
-          <div className='text-center py-10'>
+          <div className='text-center md:py-10 '>
             {children}
           </div>
         </div>
