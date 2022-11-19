6e05c722ca9824ec2cfd9cc44398a0402f11ccd7
angadgit <angadgupta214@gmail.com>
2022-11-19 10:44:30 +0530

    yarn lock remove

--- pages/api/imageUpload.jsx
+++ pages/api/imageUpload.jsx
@@ -12,17 +12,16 @@ export const config = {
 export default async (req, res) => {
   const data = await new Promise((resolve, reject) => {
     const form = new IncomingForm();
-
     form.parse(req, (err, fields, files) => {
       if (err) return reject(err);
       console.log(fields, files);
-      console.log("filepath",files.file.filepath);
+      console.log("filepath", files.file.filepath);
       var oldPath = files.file.filepath;
-      let fileName = Date.now().toString() + "_" +files.file.originalFilename;
-      console.log("filename", fileName)
-    //   var newPath = `./public/uploads/${files.file.originalFilename}`;
+      let fileName = Date.now().toString() + "_" + files.file.originalFilename;
+      console.log("filename", fileName);
+      //   var newPath = `./public/uploads/${files.file.originalFilename}`;
       var newPath = `./public/uploads/${fileName}`;
-      console.log("new path",newPath)
+      console.log("new path", newPath);
       mv(oldPath, newPath, function (err) {});
       res.status(200).json({ fields, files, newPath });
     });
